12d559710ba8cf8e40208e31c7a710ff0c73b9dc
Nick Saenko <nick.saenko@hs-osnabrueck.de>
2024-11-13 11:33:09 +0100

    Tab1/Eingang angefangen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  todos: { id: number; title: string }[] = [
+    { id: 1, title: 'Pokémon Yellow' },
+    { id: 2, title: 'Mega Man X' },
+    { id: 3, title: 'The Legend of Zelda' },
+    { id: 4, title: 'Pac-Man' },
+    { id: 5, title: 'Super Mario World' }
+  ];
+
   constructor() {}
+
+  addTodo() {
+    const newId = this.todos.length ? Math.max(...this.todos.map(todo => todo.id)) + 1 : 1;
+    this.todos.push({ id: newId, title: 'New Todo' });
+  }
+
+  editTodo(id: number) {
+    const todo = this.todos.find(todo => todo.id === id);
+    if (todo) {
+      const newTitle = prompt('Bearbeiten:', todo.title);
+      if (newTitle) {
+        todo.title = newTitle;
+      }
+    }
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
 
 }
